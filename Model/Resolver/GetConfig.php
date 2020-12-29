1871ade83b5142fe8bdce133a45fb441a4dfbed6
nghia <nghiabt@mageplaza.com>
2020-12-29 09:50:41 +0700

    update config api

---
+++ Model/Resolver/GetConfig.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_RequestForQuoteGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+declare(strict_types=1);
+
+namespace Mageplaza\RequestForQuoteGraphQl\Model\Resolver;
+
+use Exception;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\RequestForQuote\Model\Api\QuoteRepository;
+
+/**
+ * Class GetConfig
+ * @package Mageplaza\RequestForQuoteGraphQl\Model\Resolver
+ */
+class GetConfig implements ResolverInterface
+{
+
+    /**
+     * @var QuoteRepository
+     */
+    private $quoteRepository;
+
+    /**
+     * GetConfig constructor.
+     *
+     * @param QuoteRepository $quoteRepository
+     */
+    public function __construct(
+        QuoteRepository $quoteRepository
+    ) {
+        $this->quoteRepository = $quoteRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        try {
+            return [
+                'quote' => $this->quoteRepository->getConfig()
+            ];
+        } catch (Exception $e) {
+            throw new GraphQlInputException(__($e->getMessage()));
+        }
+    }
+}
